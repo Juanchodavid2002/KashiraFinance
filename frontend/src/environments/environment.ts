7ac89a31a508a977b90f4a32a5e14d7adb2eac75
Juan David <alejandroicoppo@gmail.com>
2026-08-23 21:22:21 -0500

    Fase 7: apiUrl de producción apuntando a Render

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api', // TODO: URL de producción en Render (Fase 7)
+  apiUrl: 'https://kashirafinance.onrender.com/api',
 };
